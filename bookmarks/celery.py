dabe351ea9d210fdcae569be60e758b8b9664352
Ajay263 <junioralexio607@gmail.com>
2023-08-09 09:11:00 +0200

    Add new features

--- bookmarks/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmarks.settings')
-
-app = Celery('bookmarks')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,7 +1,7 @@
-"""bookmarks URL Configuration
+"""Bookmarks URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,17 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
-    path('payment/', include('payment.urls', namespace='payment')),
-    path('', include('shop.urls', namespace='shop')),
-    
+    path('account/', include('account.urls')),
+    path('images/', include('images.urls', namespace='images')),
     
 ]
+
 if settings.DEBUG:
- urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+   urlpatterns += static(settings.MEDIA_URL,
+   document_root=settings.MEDIA_ROOT)
